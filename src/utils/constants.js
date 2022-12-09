f56241ec0db6739148cd0cac74b0b221d9d37360
Đặng Quang Dũng <92563380+dungdungptit@users.noreply.github.com>
2022-12-09 23:27:20 +0700

    logo and name app

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const logo = '../assets/images/logo_ptit.png';
-export const base_URL = process.env.REACT_APP_API_URL;
-// export const base_URL = 'http://26.2.61.248:5000';
+// export const base_URL = process.env.REACT_APP_API_URL;
+export const base_URL = 'https://api-evaluation-backend.zcode.vn';
 export const token = JSON.parse(localStorage.getItem('token'));
